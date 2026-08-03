34f103f5f102c7a200ec3def481273f78c87a445
chitrajaswanth <vtu28997@veltech.edu.in>
2026-08-03 14:55:28 +0530

    Time: 0 ms (100%), Space: 48.3 MB (50.76%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int idx = Arrays.binarySearch(nums, target);
+        return idx >= 0 ? idx : -1;
+    }
+}
